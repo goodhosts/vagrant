3f61e187776cf6c86e3440490484020fc5b935e9
Daniele Scasciafratte <mte90net@gmail.com>
2020-05-11 14:59:59 +0200

    fix #2

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p ./lib/bundle
+# Download
+curl -s https://api.github.com/repos/goodhosts/cli/releases/latest | jq --raw-output '.assets[] | .browser_download_url' | xargs -P16 wget -i
+# Extract
+tar -zxvf cli_darwin_amd64.tar.gz cli && mv cli cli_osx
+tar -zxvf cli_linux_amd64.tar.gz cli
+tar -zxvf cli_windows_amd64.tar.gz cli.exe
+rm -f ./*.tar.gz
+rm -f ./*.txt
+# Generate
+gem build vagrant-goodhosts.gemspec
