a631bbbac912ca280a915e5a5cdadd1a7ea35f23
Daniele Scasciafratte <mte90net@gmail.com>
2020-05-26 11:29:24 +0200

    package.sh updated

--- package.sh
+++ package.sh
@@ -3,11 +3,11 @@
 mkdir -p ./lib/vagrant-goodhosts/bundle
 cd ./lib/vagrant-goodhosts/bundle
 # Download
-curl -s https://api.github.com/repos/goodhosts/cli/releases/latest | jq --raw-output '.assets[] | .browser_download_url' | xargs -P16 wget -i
+curl -s https://api.github.com/repos/goodhosts/cli/releases/latest | jq --raw-output '.assets[] | .browser_download_url' | xargs wget -i
 # Extract
-tar -zxvf cli_darwin_amd64.tar.gz cli && mv cli cli_osx
-tar -zxvf cli_linux_amd64.tar.gz cli
-tar -zxvf cli_windows_amd64.tar.gz cli.exe
+tar -zxvf goodhosts_darwin_amd64.tar.gz goodhosts && mv goodhosts cli_osx
+tar -zxvf goodhosts_linux_amd64.tar.gz goodhosts && mv goodhosts cli
+tar -zxvf goodhosts_windows_amd64.tar.gz goodhosts.exe && mv goodhosts.exe cli.exe
 rm -f ./*.tar.gz
 rm -f ./*.txt
 # Generate
