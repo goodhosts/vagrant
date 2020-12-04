d1ff46767d57a839a666f635cd9f75af933cc3d5
Tom J Nowell <contact@tomjn.com>
2020-12-04 15:25:34 +0000

    Update RemoveHosts.rb

--- lib/vagrant-goodhosts/Action/RemoveHosts.rb
+++ lib/vagrant-goodhosts/Action/RemoveHosts.rb
@@ -14,11 +14,15 @@ module VagrantPlugins
           machine_action = env[:machine_action]
           if machine_action != :destroy || !@machine.id
             if machine_action != :suspend || false != @machine.config.goodhosts.remove_on_suspend
-              if machine_action != :halt || false != @machine.config.goodhosts.remove_on_suspend
-                @ui.info "[vagrant-goodhosts] Removing hosts"
-                removeHostEntries
-              else
-                @ui.info "[vagrant-goodhosts] Removing hosts on suspend disabled"
+              @first ||= false
+              unless @first
+                @first = true
+                if machine_action != :halt || false != @machine.config.goodhosts.remove_on_suspend
+                  @ui.info "[vagrant-goodhosts] Removing hosts"
+                  removeHostEntries
+                else
+                  @ui.info "[vagrant-goodhosts] Removing hosts on suspend disabled"
+                end
               end
             end
           end
