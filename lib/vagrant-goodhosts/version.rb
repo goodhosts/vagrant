3be7cb472ce1162455864b72e0186723da7ee3b3
Daniele Scasciafratte <mte90net@gmail.com>
2024-01-26 10:33:07 +0100

    feat(bump): new release

--- lib/vagrant-goodhosts/version.rb
+++ lib/vagrant-goodhosts/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module GoodHosts
-    VERSION = '1.1.6'
+    VERSION = '1.1.7'
   end
 end
--- package.sh
+++ package.sh
@@ -5,11 +5,11 @@ cd ./lib/vagrant-goodhosts/bundle
 # Download
 curl -s https://api.github.com/repos/goodhosts/cli/releases/latest | jq --raw-output '.assets[] | .browser_download_url' | xargs wget -i
 # Extract
-tar -zxvf goodhosts-1.1.0-darwin-amd64.tar.gz goodhosts && mv goodhosts cli_amd64_osx
-tar -zxvf goodhosts-1.1.0-darwin-arm64.tar.gz goodhosts && mv goodhosts cli_arm64_osx
-tar -zxvf goodhosts-1.1.0-linux-amd64.tar.gz goodhosts && mv goodhosts cli_amd64_linux
-tar -zxvf goodhosts-1.1.0-linux-arm64.tar.gz goodhosts && mv goodhosts cli_arm64_linux
-tar -zxvf goodhosts-1.1.0-windows-amd64.tar.gz goodhosts.exe && mv goodhosts.exe cli.exe
+tar -zxvf goodhosts-1.1.2-darwin-amd64.tar.gz goodhosts && mv goodhosts cli_amd64_osx
+tar -zxvf goodhosts-1.1.2-darwin-arm64.tar.gz goodhosts && mv goodhosts cli_arm64_osx
+tar -zxvf goodhosts-1.1.2-linux-amd64.tar.gz goodhosts && mv goodhosts cli_amd64_linux
+tar -zxvf goodhosts-1.1.2-linux-arm64.tar.gz goodhosts && mv goodhosts cli_arm64_linux
+tar -zxvf goodhosts-1.1.2-windows-amd64.tar.gz goodhosts.exe && mv goodhosts.exe cli.exe
 rm -f ./*.tar.gz
 rm -f ./*.txt
 # Generate
